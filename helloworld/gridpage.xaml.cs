cc76d6df438923286f5a58eeca62d30a49f64112
O G <og@Os-MacBook-Pro.local>
2020-10-13 15:01:23 +0200

    project setup

---
+++ helloworld/gridpage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace helloworld
+{
+    public partial class gridpage : ContentPage
+    {
+        public gridpage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+
+          
+        }
+
+     
+    }
+}
---
+++ helloworld/stackpage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace helloworld
+{
+    public partial class stackpage : ContentPage
+    {
+        public stackpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
